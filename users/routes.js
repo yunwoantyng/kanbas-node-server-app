5db7ba71a048ce997b128c5514c16ca4135aa0ae
雲婉庭 <yunwoantyng@yunwantingdeiMac.local>
2023-12-03 00:31:34 -0500

    a6 update

--- users/routes.js
+++ users/routes.js
@@ -1,11 +1,33 @@
 import * as dao from "./dao.js";
 let currentUser = null;
 function UserRoutes(app) {
-  const createUser = async (req, res) => {};
-  const deleteUser = async (req, res) => {};
-  const findAllUsers = async (req, res) => {};
-  const findUserById = async (req, res) => {};
-  const updateUser = async (req, res) => {};
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
+
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
+
   const signup = async (req, res) => {};
   const signin = async (req, res) => {
     const { username, password } = req.body;
