ccd078e6a1aab2330e63d0e331c7b9c359c0b2aa
雲婉庭 <yunwoantyng@yunwantingdeiMac.local>
2023-12-02 19:26:17 -0500

    try users

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 app.use(cors()); // any request can access this server
 app.use(express.json()); // parse json body
 
+UserRoutes(app);
 Lab5(app);
 ModuleRoutes(app);
 CourseRoutes(app);
