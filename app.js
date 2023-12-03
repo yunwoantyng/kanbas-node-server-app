aa7ad5afc433695c4ef4bd697b143b6d99faaa78
雲婉庭 <yunwoantyng@yunwantingdeiMac.local>
2023-12-03 02:58:25 -0500

    a6 done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // const express = require("express");
+
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
@@ -9,13 +10,15 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   })
 );
 // any request can access this server
@@ -24,6 +27,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 
 app.use(express.json()); // parse json body
